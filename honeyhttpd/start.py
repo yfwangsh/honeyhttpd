6b6c544f6fb9fbc6449190fc4bb3fe37144527a3
yfwangsh <yfwangsh@citiz.net>
2019-06-04 14:03:49 +0800

    fix typo

--- honeyhttpd/start.py
+++ honeyhttpd/start.py
@@ -103,7 +103,7 @@ class ServerManager(object):
                      sys.exit(3)
                 print("Starting https on port " + str(server_config['port']))
                 server = server_module(server_config['domain'], int(server_config['port']), server_config['timeout'], wait, self.__loggers, server_config['cert_path'])
-                elif server_config['mode'] == "proxy":
+            elif server_config['mode'] == "proxy":
                 if "config" not in server_config:
                      self.error("config is not complete set for proxy " + handler)
                      sys.exit(3)
